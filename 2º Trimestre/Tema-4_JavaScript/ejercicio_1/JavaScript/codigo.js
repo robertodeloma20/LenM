46eeb02dac610385fdb1314a55244ff23ecdf293
robertodeloma20 <a_robdelsal@iestriana.com>
2023-03-24 16:19:49 +0100

    lo

---
+++ 2º Trimestre/Tema-4_JavaScript/ejercicio_1/JavaScript/codigo.js
@@ -0,0 +1,4 @@
+function ponerRojo() {
+    let imagen = document.getElementById("imgBoton")
+    imagen.src = "./"
+}
---
+++ 2º Trimestre/Tema-4_JavaScript/ejercicio_2/JS/style.js
@@ -0,0 +1,7 @@
+function mostrarParrafo(){
+    let enlace = document.getElementById("btnMostrar");
+    let parrafo = document.getElementById("segundoParrafo");
+
+    enlace.className = "oculto";
+    parrafo.className = "";
+}
